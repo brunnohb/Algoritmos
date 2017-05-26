96e40f1b8cd37d5d292f705990e4c88dc49704fd
Noturno <Noturno@192.168.7.29>
2017-05-25 21:53:16 -0300

    Exercicio 5

--- Algoritmos/src/Exercicio_Condicionais_e_Interadores/Exercicio_5.java
+++ Algoritmos/src/Exercicio_Condicionais_e_Interadores/Exercicio_5.java
@@ -4,9 +4,26 @@ package Exercicio_Condicionais_e_Interadores;
  * usuário, ou seja, se realmente ele tem a idade que disse ter. Caso sim imprima 
  * “Você não mentiu para mim”, caso contrário, imprima “Você mentiu para mim, você 
  * não é mais meu amigo”.*/
+
+import javax.swing.JOptionPane;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Exercicio_5 {
 	public static void main(String[] args) {
-		
+
+		int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade: "));
+		int anoNasc = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano que você nasceu: "));
+
+		Calendar cal = GregorianCalendar.getInstance();
+		int anoAtual = cal.get(Calendar.YEAR);
+
+		if (idade == (anoAtual - anoNasc)) {
+			JOptionPane.showMessageDialog(null, "Você não mentiu para mim, para o seu bem.");
+		} else {
+			JOptionPane.showMessageDialog(null, "Você mentiu para mim, você não é mais meu amigo.");
+		}
+
 	}
 
 }
